f4d0e84658bacea65be3264ebfe83f762234934d
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-01-18 19:39:44 +0100

    Basic app.js

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+require('dotenv').config()
+
+const port = process.env.PORT || 4000
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('Official E-Commerce Page');
+});
+
+app.listen(port, () => console.log("Server running"));
+
+module.exports = app;
